4446ab1c808c3e8b49cbd92ba19f4c46657c1369
Jimmy Thrasibule <dev@jimmy.lt>
2025-01-06 23:14:46 +0100

    flake: Add Nix stable compatibility
    Use `flake-compat <https://github.com/edolstra/flake-compat>`_ to ensure
    compatibility with Nix stable.
    
    Signed-off-by: Jimmy Thrasibule <dev@jimmy.lt>

---
+++ default.nix
@@ -0,0 +1,34 @@
+# default.nix
+# ===========
+#
+# Copying
+# -------
+#
+# Copyright (c) 2025 universe/config authors and contributors.
+#
+# This file is part of the *universe/config* project.
+#
+# *universe/config* is a free software project. You can redistribute it
+# and/or modify it following the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY KIND;
+# including but not limited to the WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with
+# *universe/config*. If not, see <http://opensource.org/licenses/MIT>.
+#
+(
+  import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  )
+  {src = ./.;}
+)
+.defaultNix
