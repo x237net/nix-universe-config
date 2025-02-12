03b4fe4b4148d143f098edb4224b4e2371160f2d
Jimmy Thrasibule <dev@jimmy.lt>
2025-02-12 17:26:37 +0100

    shells/http: Create a shell for HTTP queries
    Create a shell to query and investigate HTTP services.
    
    Signed-off-by: Jimmy Thrasibule <dev@jimmy.lt>

---
+++ shells/http/default.nix
@@ -0,0 +1,62 @@
+# shells/http/default.nix
+# =======================
+#
+# Copying
+# -------
+#
+# Copyright (c) 2025 universe/config authors and contributors.
+#
+# This file is part of the *universe/config* project.
+#
+# *universe/config* is a free software project. You can redistribute it
+# and/or modify it following the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY KIND;
+# including but not limited to the WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with
+# *universe/config*. If not, see <http://opensource.org/licenses/MIT>.
+#
+{
+  # Flake's library as well as the libraries available from the flake's
+  # inputs.
+  lib,
+  # Flake's inputs.
+  inputs,
+  # The namespace of the flake. See `snowfall.namespace`.
+  namespace,
+  # An instance of `pkgs` with the overlays and other packages.
+  pkgs,
+  mkShell,
+  ...
+}: let
+  shell = builtins.baseNameOf (builtins.getEnv "SHELL");
+in
+  mkShell {
+    packages = with pkgs; [
+      bruno
+      bruno-cli
+      curl
+      dnsutils
+      httpx
+      katana
+      openssl
+      playwright-driver
+
+      (python3.withPackages (
+        pypkgs:
+          with pypkgs; [
+            httpx
+            playwright
+            requests
+          ]
+      ))
+
+      "${shell}"
+    ];
+
+    shellHook = ''
+      exec ${shell}
+    '';
+  }
