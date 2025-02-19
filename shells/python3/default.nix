e1b82b11cde48eb01dc6772241a3ffe0df50d001
Jimmy Thrasibule <dev@jimmy.lt>
2025-02-19 17:02:35 +0100

    shell/python3: Add a shell to develop with Python 3
    The shell creates a Python 3 environment with common tools to start a Python
    project. The shell also allows other Python versions to be installed using
    `pyenv <https://github.com/pyenv/pyenv>`_.
    
    Signed-off-by: Jimmy Thrasibule <dev@jimmy.lt>

---
+++ shells/python3/default.nix
@@ -0,0 +1,65 @@
+# shells/python/default.nix
+# =========================
+#
+# Copying
+# -------
+#
+# Copyright (c) 2025 universe/config authors and contributors.
+#
+# This file is part of the *universe/config* project.
+#
+# *universe/config* is a free software project. You can redistribute it
+# and/or modify it following the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY KIND;
+# including but not limited to the WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with
+# *universe/config*. If not, see <http://opensource.org/licenses/MIT>.
+#
+{
+  # Flake's library as well as the libraries available from the flake's
+  # inputs.
+  lib,
+  # Flake's inputs.
+  inputs,
+  # The namespace of the flake. See `snowfall.namespace`.
+  namespace,
+  # An instance of `pkgs` with the overlays and other packages.
+  pkgs,
+  mkShell,
+  ...
+}: let
+  shell = builtins.baseNameOf (builtins.getEnv "SHELL");
+in
+  mkShell {
+    packages = with pkgs;
+      [
+        python3
+        black
+        cookiecutter
+        git
+        isort
+        mypy
+        poetry
+        pyenv
+        ruff
+        sphinx
+        uv
+
+        "${shell}"
+      ]
+      ++ pkgs.python3.buildInputs
+      ++ [
+        libb2
+        libuuid
+        tcl
+        tk
+      ];
+
+    PYTHON_CONFIGURE_OPTS = builtins.concatStringsSep " " pkgs.python3.configureFlags;
+    shellHook = ''
+      exec ${shell}
+    '';
+  }
