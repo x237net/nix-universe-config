6d67965e010c6dc63e605a9a6ce234fa62110b4e
Jimmy Thrasibule <dev@jimmy.lt>
2025-02-21 19:12:06 +0100

    module/firefox: Add a module to manage Firefox
    `Firefox <https://www.mozilla.org/firefox/>`_ is a free and open source web
    browser developed by Mozilla.
    
    Signed-off-by: Jimmy Thrasibule <dev@jimmy.lt>

---
+++ lib/apps/firefox/default.nix
@@ -0,0 +1,44 @@
+# lib/apps/firefox/default.nix
+# ============================
+#
+# Copying
+# -------
+#
+# Copyright (c) 2025 Thrasibule.Mx - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+#
+{
+  # Flake's library as well as the libraries available from the flake's
+  # inputs.
+  lib,
+  # Flake's inputs.
+  inputs,
+  # The namespace of the flake. See `snowfall.namespace`.
+  namespace,
+  ...
+}: {
+  apps.firefox = {
+    policy = import ./policy.nix {inherit lib inputs namespace;};
+
+    mkOptions = with lib;
+    with lib.types; {
+      enable = mkEnableOption "Whether to enable management of Firefox.";
+
+      package = mkOption {
+        description = "The Firefox package to install.";
+        default = pkgs.firefox;
+        type = nullOr package;
+      };
+
+      packageInstall = mkOption {
+        description = ''
+          Whether to install the Firefox package as part of the user profile.
+        '';
+        default = !isDarwin;
+        type = bool;
+      };
+    };
+  };
+}
---
+++ modules/darwin/firefox/default.nix
@@ -0,0 +1,59 @@
+# modules/darwin/firefox/default.nix
+# ==================================
+#
+# Copying
+# -------
+#
+# Copyright (c) 2025 universe/config authors and contributors.
+#
+# This file is part of the *universe/config* project.
+#
+# *universe/config* is a free software project. You can redistribute it
+# and/or modify it following the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY KIND;
+# including but not limited to the WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with
+# *universe/config*. If not, see <http://opensource.org/licenses/MIT>.
+#
+{
+  # Flake's library as well as the libraries available from the flake's
+  # inputs.
+  lib,
+  # An instance of input packages mixed with the flake's overlays and packages.
+  pkgs,
+  # Flake's inputs.
+  inputs,
+  # The namespace of the flake. See `snowfall.namespace`.
+  namespace,
+  # The system architecture for the system (eg. `x86_64-linux`).
+  system,
+  # The Snowfall Lib target for the system (eg. `x86_64-iso`).
+  target,
+  # A normalized name for the system target (eg. `iso`).
+  format,
+  # A boolean to determine whether this system is a virtual target using
+  # nixos-generators.
+  virtual,
+  # An attribute map of other defined systems.
+  systems,
+  # Options coming from user of the module.
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.universe.darwin.firefox;
+
+  package = if cfg.packageInstall then cfg.package else null;
+in {
+  imports = universe.fs.import-directory ./.;
+
+  options.universe.darwin.firefox = universe.apps.firefox.mkOptions;
+
+  config = mkIf cfg.enable {
+    inherit (cfg) enable;
+    inherit package;
+  };
+}
